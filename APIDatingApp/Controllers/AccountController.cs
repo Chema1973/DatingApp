d8bebcafc274048e0bd93549ccbd867820e1c4e6
Chema1973 <chema.pairet@gmail.com>
2023-05-09 16:33:30 +0200

    End of Section 12

--- APIDatingApp/Controllers/AccountController.cs
+++ APIDatingApp/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using APIDatingApp.Data;
 using APIDatingApp.DTOs;
 using APIDatingApp.Entities;
 using APIDatingApp.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,13 +13,15 @@ namespace APIDatingApp.Controllers
     public class AccountController : BaseApiController
     {
         private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
         private readonly DataContext _context;
         private readonly ITokenService _tokenService;
 
-        public AccountController(DataContext context, ITokenService tokenService, IUserRepository userRepository){
+        public AccountController(DataContext context, ITokenService tokenService, IUserRepository userRepository, IMapper mapper){
             _context = context;
             _tokenService = tokenService;
             _userRepository = userRepository;
+            _mapper = mapper;
         }
 
         [HttpPost("register")]
@@ -27,13 +30,21 @@ namespace APIDatingApp.Controllers
 
             if (await UserExists(registerDTO.UserName)) return BadRequest("Username is taken");
 
+            var user = _mapper.Map<AppUser>(registerDTO);
+
+
             using var hmac = new HMACSHA512();
 
-            var user = new AppUser{
+            user.UserName = registerDTO.UserName.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password));
+            user.PasswordSalt = hmac.Key;
+
+            /*var user = new AppUser{
                 UserName = registerDTO.UserName.ToLower(),
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
                 PasswordSalt = hmac.Key
-            };
+            };*/
+
 
             _context.Users.Add(user);
 
@@ -41,7 +52,8 @@ namespace APIDatingApp.Controllers
 
             return new UserDto{
                 UserName = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                KnownAs = user.KnownAs
             };
         }
 
@@ -68,7 +80,8 @@ namespace APIDatingApp.Controllers
             return new UserDto{
                 UserName = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+                KnownAs = user.KnownAs
             };
 
             // return user;
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -14,25 +16,94 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   // Del HIJO llamaremos al PADRE
 
-  model: any = {};
+  // model: any = {};
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) {}
+  // Formularios Reactivos
+  // registerForm: FormGroup | undefined;
+  registerForm: FormGroup = new FormGroup({});
+
+  maxDate: Date = new Date();
+  validationErrors: string[] | undefined;
+
+
+  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
-    
+    this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  initializeForm()
+  {
+    // Reemplazamos "new FormGroup" por "this.fb.group" y simplificamos un poco más el código
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]],
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+/*
+  initializeForm2()
+  {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
+    });
+    // Esto se hace para que cuando cambie el valor de "password" (nos suscribimos a su evento "valueChange"),
+    // automaticamente se vuelva a validar el campo "confirmPassword"
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+*/
+  matchValues(matchTo: string) : ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true}
+      // "notMatching" la usaremos en el front para los mensajes de error
+    };
   }
 
   register() {
-    console.log(this.model);
-    this.accountService.register(this.model).subscribe({
+    console.log(this.registerForm?.value);
+    const dob = this.getDateOnly(this.registerForm.controls['dateOfBirth'].value);
+    const values = {...this.registerForm.value, dateOfBirth: dob};
+    console.log(values);
+
+    // let numeros = [10, 4, 7, 15, 3, 25];
+    // console.log(numeros);     // Imprime un array.
+    // console.log(...numeros);  // Imprime una lista de argumentos.
+    // Al anteponer los tres puntos que representan al spread operator transformamos la variable numeros
+    // (que en el ejemplo representa un array con números) en una lista de argumentos, y es por ello que
+    // podemos acceder al número mayor del array numeros. Es como si le quitáramos los corchetes
+    // ( “[]” ) al array.
+
+    // console.log(this.model);
+
+    // this.accountService.register(this.model).subscribe({
+      this.accountService.register(values).subscribe({
       next: () => {
       // next: response => {
         // console.log(response);
         // --> Si "register" de "accountService" devuelve el "user"
         //     lo que pintaría "response" sería un "UserDTO"
-        this.cancel();
-      }, error: error => this.toastr.error(error.error)
+        // this.cancel();
+        this.router.navigateByUrl('/members')
+      }, error: error => {
+        // this.toastr.error(error.error)
+        // console.log(error);
+        this.validationErrors = error
+      }
     })
+    
   }
 
   cancel(){
@@ -41,6 +112,13 @@ export class RegisterComponent implements OnInit {
     // --> Enviamos el evento al PADRE
     //     En este caso, en el "home.component.html" -> (cancelRegister)="cancelRegisterMode($event)"
     //     home.component.ts : cancelRegisterMode
+  }
+
+  private getDateOnly(dob: string | undefined) {
+    if (!dob) return;
+
+    let theDob = new Date(dob);
+    return new Date(theDob.setMinutes(theDob.getMinutes() - theDob.getTimezoneOffset())).toISOString().slice(0,10);
   }
 
 }
